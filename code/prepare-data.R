89072647fbbd6941ff3b4b092fb5758d4ed1d70a
jackos13 <38255542+jackos13@users.noreply.github.com>
2019-06-19 15:24:08 +0100

    Update prepare-data.R

--- code/prepare-data.R
+++ code/prepare-data.R
@@ -1,8 +1,9 @@
-############################################################################################
-########################### Functions to prep the data #####################################
-############################################################################################
+########################################################################
+# Functions to prepare to run the PP GPD:
+########################################################################
 
-########################### Getting all potential betas
+########################################################################
+# Getting all potential betas
 get_betas <- function(beta.vec, spatial.dim) {
   # Getting the number of unique elements in the symmetric
   # matrix, using n(n+1)/2
@@ -43,9 +44,10 @@ get_betas <- function(beta.vec, spatial.dim) {
   return(list(big_beta, big_beta_mat))
 }
 
-########################### Getting the big cubes
+########################################################################
+# Getting the big cubes
 # Create the cubes (if needed):
-save_cubes <- function(addresses) {
+save_cubes <- function(addresses, coords.m, coords.n, big_beta, prior, start) {
   
   # Creating the scale and shape covariances:
   dist <- dist_mat_diff(coords.m, coords.m)
@@ -60,8 +62,103 @@ save_cubes <- function(addresses) {
                 start$sigsq_scale, start$tausq_scale, addresses[3])
   save_big_cube(big_dist, big_beta, prior$nu_shape_hyper_discrete,
                 start$sigsq_shape, start$tausq_shape, addresses[4])
+
+}
+
+########################################################################
+# Find the initial values for the algorithm:
+find.init <- function(start, it.max, addresses, m, n, data,
+                      bigcov1, bigcov2, cov1, cov2) {
   
-  # Now to reset the tausq parameters after the cubes have been created:
-  start$tausq_scale <- 0.01; start$tausq_shape <- 0.01
+  # Step a:
+  # Read in the cubes created above:
+  cube_list <- retrieve_cube(addresses)
+  names(cube_list) <- c('cube_covar_scale',
+                        'cube_covar_shape',
+                        'cube_nxm_scale',
+                        'cube_nxm_shape')
   
+  # Step b:
+  # Packages needed:
+  library(mvtnorm)
+  library(boot)
+  library(fields)
+  library(ismev)
+  
+  # Step c:
+  # Getting the log-scale first:
+  # Defining everything needed:
+  alpha1 <- start$alpha_scale
+  n_x_m1 <- cube_list$cube_nxm_scale[,,1]
+  Sigma1 <- cube_list$cube_covar_scale[,,1]
+  ldett1 <- log(det(Sigma1))
+  data1 <- data
+  shape <- rep(0, length(data))
+  
+  # I want to maximise post_scaleC
+  post_scale_negll <- function(mlscale) {
+    
+    lscale <- bigcov1 %*% alpha1 + n_x_m1 %*% solve(Sigma1, mlscale - cov1 %*% alpha1)
+    
+    # Adding in this to allow the shape to be 0:
+    count <- 0
+    for(i in 1:n) {
+      count <- count + sum(texmex::dgpd(data[[i]], exp(lscale)[i], shape[i], 0, log.d = TRUE))
+    }
+    
+    b <- gp_mvnC2(mlscale, Sigma1, alpha1, cov1, ldett1)
+    
+    return(-(count + b))
+  }
+  
+  # Now optimise it:
+  answer <- nlminb(start = rep(0.1, m), 
+                   objective = post_scale_negll,
+                   control = list(iter.max = it.max))
+  
+  # Now to find the full vector from this:
+  new.mlscale <- answer$par
+  new.lscale <- bigcov1 %*% alpha1 + n_x_m1 %*% solve(Sigma1, new.mlscale - cov1 %*% alpha1)
+  
+  # Step d:
+  # Now to find a corresponding one for the shape:
+  ### Now to find values for the shape:
+  alpha2 <- start$alpha_shape
+  n_x_m2 <- cube_list$cube_nxm_shape[,,1]
+  Sigma2 <- cube_list$cube_covar_shape[,,1]
+  ldett2 <- log(det(Sigma2))
+  
+  # I want to maximise post_shapeC
+  post_shape_negll <- function(mshape) {
+    
+    shape <- bigcov2 %*% alpha2 + n_x_m2 %*% solve(Sigma2, mshape - cov2 %*% alpha2)
+    
+    # Adding in this to allow the shape to be 0:
+    count <- 0
+    for(i in 1:n) {
+      count <- count + sum(texmex::dgpd(data[[i]], exp(new.lscale)[i], shape[i], 0, log.d = TRUE))
+    }
+    
+    b <- gp_mvnC2(mshape, Sigma2, alpha2, cov2, ldett2)
+    
+    return(-(count + b))
+  }
+  
+  # Now optimise it:
+  answer2 <- nlminb(start = rep(1, m), 
+                    objective = post_shape_negll,
+                    control = list(iter.max = it.max))
+  
+  # Now to find the full vector from this:
+  new.mshape <- answer2$par
+  new.shape <- bigcov2 %*% alpha2 + n_x_m2 %*% solve(Sigma2, new.mshape - cov2 %*% alpha2)
+  
+  # Step e:
+  # Now to replace the parts of start:
+  start$lscale <- new.mlscale
+  start$shape <- new.mshape
+  start$nlscale <- new.lscale
+  start$nshape <- new.shape
+  
+  return(start)
 }
